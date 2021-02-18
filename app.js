329fb7f04dee0731673b2a7b4fe323248693f949
misha <trickerforyou@gmail.com>
2021-02-18 21:00:28 +0200

    commit2 remade

--- app.js
+++ app.js
@@ -1,67 +1,60 @@
 const fs = require('fs');
 const path = require('path');
+const sixPM = path.join(__dirname, 'dir', '/1800');
+const eightPM = path.join(__dirname, 'dir', '/2000');
 
-const dirdir = path.join(__dirname, '/dir');
-
-
-function genders() {
-    fs.readdir(dirdir, ((err, files) => {
+function sort(sixPM, eightPM) {
+    fs.readdir(eightPM, (err, files) => {
         if (err) {
             console.log(err);
             return;
         }
-        console.log(files);
 
-        files.forEach(fileName => {
-            console.log(fileName);
-
-            fs.readdir(path.join(dirdir, `${fileName}`), (err, files1) => {
-                if (err) {
-                    console.log(err);
+        files.forEach(file => {
+            fs.readFile(path.join(eightPM, file), (err1, data) => {
+                if (err1) {
+                    console.log(err1);
                     return;
                 }
-                console.log(files1);
 
-                files1.forEach(file1Name => {
-                    let filePath = path.join(dirdir, `${fileName}`, `${file1Name}`);
-                    fs.readFile(filePath, (err, data) => {
-                        if (err) {
-                            console.log(err);
-                            return;
+                const json = JSON.parse(data.toString());
+
+                if (json.gender === 'female') {
+                    fs.rename(path.join(eightPM, file), path.join(sixPM, file), err2 => {
+                        if (err2) {
+                            console.log(err2);
                         }
-                        const json = JSON.parse(data.toString());
-                        const malegender = path.join(dirdir, '1800');
-                        const femalegender = path.join(dirdir, '2000');
-
-                        if (json.gender === 'male') {
-
-                            fs.rename(filePath, path.join(malegender, `${file1Name}`), err => {
-                                if (err) {
-                                    console.log(err);
-                                    return;
-                                }
-                            })
-
-                        } else if (json.gender === 'female') {
-
-                            fs.rename(filePath, path.join(femalegender, `${file1Name}`), err => {
-                                if (err) {
-                                    console.log(err);
-                                    return;
-                                }
-                            })
-
-
-                        }
-
                     })
-                })
-
+                }
             })
         })
+    })
 
-    }))
+    fs.readdir(sixPM, (err, files) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
 
+        files.forEach(file => {
+            fs.readFile(path.join(sixPM, file), (err1, data) => {
+                if (err1) {
+                    console.log(err1);
+                    return;
+                }
+
+                const json = JSON.parse(data.toString());
+
+                if (json.gender === 'male') {
+                    fs.rename(path.join(sixPM, file), path.join(eightPM, file), err2 => {
+                        if (err2) {
+                            console.log(err2);
+                        }
+                    })
+                }
+            })
+        })
+    })
 }
 
-genders();
+sort(sixPM, eightPM);
